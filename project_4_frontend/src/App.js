1acc9588e36adbfeb90ba9ef4640c7f49accdc09
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-03-03 22:16:34 -0500

    got post to show

--- project_4_frontend/src/App.js
+++ project_4_frontend/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import MessageBoard from './components/MessageBoard.js'
+import Auth from './modules/Auth'
+import PostList from './components/PostList'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      auth: Auth.isUserAuthenticated()
+    }
+  }
 
   render() {
     return (
       <div>
       <MessageBoard />
-
+      <PostList />
       </div>
 
     );
