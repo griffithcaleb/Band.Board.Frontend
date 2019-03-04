2e2aa769785973a6c2f04444556aefdbedd828a3
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-03-03 23:24:56 -0500

    can now authenticate user

--- project_4_frontend/src/App.js
+++ project_4_frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Auth from './modules/Auth'
 import Nav from './components/Nav'
+import SignUp from './components/SignUp'
 
 class App extends Component {
   constructor(props){
@@ -10,11 +11,37 @@ class App extends Component {
     }
   }
 
+  signUp = (e,data) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/users',{
+      method:'POST',
+      body: JSON.stringify({user: data}),
+      headers:{
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((res) => {
+      res.json()
+      .then((data) => {
+        Auth.authenticateToken(data.token)
+        this.setState({
+          auth: Auth.isUserAuthenticated()
+        })
+      },(err) => {
+        console.log(err);
+      })
+    })
+  }
+
   render() {
     return (
       <div>
-      <Nav />
-      
+      {this.state.auth? <Nav />:
+        <div>
+          <SignUp signUp={this.signUp}/>
+        </div>
+       }
       </div>
 
     );
---
+++ project_4_frontend/src/components/SignUp.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+class SignUp extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username: "",
+      password:"",
+      email:"",
+      name:''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e){
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  render(){
+    return(
+      <>
+      <form onSubmit={(e)=>this.props.signUp(e,this.state)}>
+        <input type="text" name="username" onChange={this.handleChange}
+         value={this.state.username} placeholder="Username"/>
+        <input type="password" name="password" onChange={this.handleChange}
+         value={this.state.password} placeholder="Password"/>
+        <input type="email" name="email" onChange={this.handleChange}
+         value={this.state.email} placeholder="Email"/>
+        <input type="text" name="name" onChange={this.handleChange}
+         value={this.state.name} placeholder="Name"/>
+         <input type="submit"/>
+      </form>
+      </>
+    )
+  }
+}
+
+
+export default SignUp
