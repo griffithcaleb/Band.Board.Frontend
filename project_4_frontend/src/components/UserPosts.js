48342d1dbdf27739b2a65ed752b540916a293330
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-03-04 14:49:23 -0500

    can now delete

--- project_4_frontend/src/components/UserPosts.js
+++ project_4_frontend/src/components/UserPosts.js
@@ -26,15 +26,42 @@ class UserPost extends Component {
     })
   }
 
+  deletePost = (id, index) => {
+    fetch("http://localhost:3000/posts/"+id,{
+      method: 'DELETE',
+      headers:{
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        token: Auth.getToken(),
+        'Authorization': `Token ${Auth.getToken()}`
+      }
+    }).then((res) => {
+      this.removeFromArr(index)
+    })
+  }
+
+  removeFromArr = (index) => {
+    this.setState((prevState) => {
+      prevState.myPosts.splice(index,1)
+      return{
+        myPosts: prevState.myPosts
+      }
+    })
+  }
+
   render(){
     return(
       <>
       <h2>Your Posts</h2>
-      {this.state.myPosts.map((post) => {
+      {this.state.myPosts.map((post,index) => {
         return(
           <div key={post.id}>
             <h2>{post.title}</h2>
-              <p>{post.info}</p>
+              <p>{post.info}
+              <span
+              onClick={()=>this.deletePost(post.id,index)}
+              >   X</span>
+              </p>
 
           </div>
         )
